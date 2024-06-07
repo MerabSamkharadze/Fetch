e02a1e3a571a2aa14506f4215a33d1a457341c87
Merabi Samkharadze <samkharadzemerab@gmail.com>
2024-06-07 20:21:49 +0400

    get infro by fetch method

---
+++ script.js
@@ -0,0 +1,27 @@
+const ul = document.getElementById("ul");
+
+fetch("https://jsonplaceholder.typicode.com/users", { method: "GET" })
+  .then(function (responseinfo) {
+    if (!responseinfo.ok) {
+      throw responseinfo.status;
+    }
+    return responseinfo.json();
+  })
+  .then(function (responseData) {
+    responseData.forEach((item) => {
+      const li = document.createElement("li");
+      li.textContent = item.company.name;
+      ul.appendChild(li);
+    });
+  })
+  .catch(function (error) {
+    if (error === 404) {
+      const li = document.createElement("li");
+      li.textContent = "Page not found";
+      ul.appendChild(li);
+    } else {
+      const li = document.createElement("li");
+      li.textContent = "Server Error";
+      divElement.appendChild(li);
+    }
+  });
